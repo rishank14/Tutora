2c1a581e2fe0c42d4ed3ba85cd515355afad1210
rishank14 <rishankkalra@gmail.com>
2025-10-10 12:02:10 +0530

    Implement Navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,29 @@
 import type { Metadata } from "next";
 import { Bricolage_Grotesque } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
 const bricolage = Bricolage_Grotesque({
-  variable: "--font-bricolage",
-  subsets: ["latin"],
+   variable: "--font-bricolage",
+   subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "Tutora",
-  description: "Real-time AI Teaching Platform",
-  icons: {
-      icon: "/images/logo.png",
-      shortcut: "/images/logo.png",
-      apple: "/images/logo.png",
-   },
+   title: "Converso",
+   description: "Real-time AI Teaching Platform",
 };
 
 export default function RootLayout({
-  children,
+   children,
 }: Readonly<{
-  children: React.ReactNode;
+   children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={`${bricolage.variable} antialiased`}>{children}</body>
-    </html>
-  );
+   return (
+      <html lang="en">
+         <body className={`${bricolage.variable} antialiased`}>
+            <Navbar />
+            {children}
+         </body>
+      </html>
+   );
 }
