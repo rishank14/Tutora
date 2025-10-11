e40ca5c4573c709185f8a633dcb590bf89548221
rishank14 <rishankkalra@gmail.com>
2025-10-11 19:32:02 +0530

    Add Companion Builder and UI components for companion creation and listing

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,27 @@
-import { Button } from '@/components/ui/button'
+import CompanionCard from '@/components/CompanionCard'
+import CompanionsList from '@/components/CompanionsList'
+import CTA from '@/components/CTA'
+import { recentSessions } from '@/constants'
 import React from 'react'
 
 const Page = () => {
   return (
-    <div>
-      <h1 className='text-2xl underline'>Welcome</h1>
-      <Button>
-        Click me
-      </Button>
-    </div>
+    <main>
+      <h1>Popular Companions</h1>
+      <section className='home-section'>
+        <CompanionCard />
+        <CompanionCard />
+        <CompanionCard />
+      </section>
+
+      <section className='home-section'><CompanionsList
+      title='Recently completed sessions'
+      companions={recentSessions}
+      classNames='w-2/3 max-lg:w-full'
+      />
+      <CTA />
+      </section>
+    </main>
   )
 }
 
